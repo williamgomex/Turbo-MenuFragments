1a53c2f41eb7c8d315e49f851ad34a9f14f67fc7
William Gómez O <william.gomex@hotmail.com>
2015-05-14 21:13:16 -0500

    Idioma inglés actualizado.

--- app/src/main/java/com/zoomcreativo/turbo_menufragments/Acercade.java
+++ app/src/main/java/com/zoomcreativo/turbo_menufragments/Acercade.java
@@ -12,11 +12,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class Acercade  extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if(Locale.getDefault().getLanguage().equals("sp"))
+        {
         getDialog().setTitle("Acerca de...");
+        }
+        else
+        {
+        getDialog().setTitle("About ...");
+        }
         return inflater.inflate(R.layout.fragment_acercade,null);
     }
 }
